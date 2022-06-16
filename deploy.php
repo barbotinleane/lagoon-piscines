c9e5b574ec425af27bbf19334f79597dba930cc5
Léane Barbotin <barbotinleane@gmail.com>
2022-06-16 16:40:07 +0200

    Adding part réalisez votre reve in homepage

--- deploy.php
+++ deploy.php
@@ -19,7 +19,7 @@ host('lagoonpiscines')
     ->set('remote_user', 'lagoonpiko')
     ->set('http_user', 'lagoonpiko')
     ->set('deploy_path', '~/lagoonsymfony')
-    ->set('branch', 'main')
+    ->set('branch', 'new-design')
     ->set('writable_mode', 'chmod');
 
 // Tasks

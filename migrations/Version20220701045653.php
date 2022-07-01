cc80bd3b2403aeabbdde127121d24e0c08812383
Léane Barbotin <barbotinleane@gmail.com>
2022-07-01 08:02:08 +0200

    Bugfix in migrations with table user

--- migrations/Version20220701045653.php
+++ migrations/Version20220701045653.php
@@ -21,7 +21,6 @@ final class Version20220701045653 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE formation_categories (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE formation_libelles ADD category_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE formation_libelles ADD CONSTRAINT FK_CE8FA7AD12469DE2 FOREIGN KEY (category_id) REFERENCES formation_categories (id)');
         $this->addSql('CREATE INDEX IDX_CE8FA7AD12469DE2 ON formation_libelles (category_id)');

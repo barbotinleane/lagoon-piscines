8573087327692f2fb08d22d445c4791c8052050e
Léane Barbotin <barbotinleane@gmail.com>
2022-12-05 19:29:39 +0100

    big fixed with asana

--- src/Service/AsanaManager.php
+++ src/Service/AsanaManager.php
@@ -69,7 +69,6 @@ class AsanaManager
                 ],
                 'fields' => ['html_notes'],
             ));
-            dump($result);
         } catch (\Exception $e) {
             return new RedirectResponse($this->router->generate('app_404_error'));
         }

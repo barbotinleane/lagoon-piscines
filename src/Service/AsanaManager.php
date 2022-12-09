5029305fea46887e905a08701d48eceb9199d96b
Léane Barbotin <barbotinleane@gmail.com>
2022-12-09 13:14:36 +0100

    Display error for asana manager

--- src/Service/AsanaManager.php
+++ src/Service/AsanaManager.php
@@ -69,6 +69,7 @@ class AsanaManager
                 ],
                 'fields' => ['html_notes'],
             ));
+            dump($result);
         } catch (\Exception $e) {
             return new RedirectResponse($this->router->generate('app_404_error'));
         }

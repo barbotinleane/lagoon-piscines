2eead961ff26e50cbf461782953be25fe268ddbc
Léane Barbotin <barbotinleane@gmail.com>
2023-02-17 12:24:04 +0100

    Prepare deploy to prod

--- deploy.php
+++ deploy.php
@@ -18,7 +18,7 @@ add('writable_dirs', []);
 host('lagoonpiscines')
     ->set('remote_user', 'lagoonpiko')
     ->set('http_user', 'lagoonpiko')
-    ->set('deploy_path', '~/testlagoonsymfony')
+    ->set('deploy_path', '~/lagoonsymfony')
     ->set('branch', 'newform')
     ->set('writable_mode', 'chmod');
 
